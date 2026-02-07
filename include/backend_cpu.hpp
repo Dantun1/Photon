c965ace0b7d1d7d8491317a17c0a16b74783f6f3
Dantun1 <daniel@tunyan.com>
2026-02-07 12:22:45 +0000

    Define + Implement NDArray class and some of its constructors; enhance CompactArray constructor for move semantics

--- include/backend_cpu.hpp
+++ include/backend_cpu.hpp
@@ -2,6 +2,12 @@
 #include <vector>
 #include <cstddef>
 
+/**
+ * @brief A compact array class that manages contiguous block of memory for a single data type. This is the underlying storage for NDArray.
+ * Just a std::vector wrapper for now, but can be extended to support GPU memoryn in the future.
+ *
+ * @tparam T The numeric data type of the array elements.
+ */
 template <typename T>
 class CompactArray
 {
@@ -19,4 +25,37 @@ public:
     // pointers to data for gpu api.
     T *ptr();
     const T *ptr() const;
+};
+
+/**
+ * @brief A multi-dimensional array class that provides a high-level interface for working with n-dimensional data.
+ * * It manages the shape, strides, and offset for efficient indexing and slicing.
+ * The actual data is stored in a CompactArray, which can be shared across multiple NDArray instances to enable views and slicing without copying data.
+ *
+ *
+ * @tparam T The numeric data type of the array elements.
+ */
+template <typename T>
+class NDArray
+{
+    // Manage the data via shared_ptr
+    std::shared_ptr<CompactArray<T>> handle;
+    std::vector<size_t> shape;
+    std::vector<size_t> strides;
+    size_t offset;
+
+public:
+    // Zeroed NDArray of shape
+    explicit NDArray(const std::vector<size_t> &shape);
+    // Create ndarray from existing vector + shape
+    NDArray(std::vector<T> data, const std::vector<size_t> shape);
+    // Construct new view of existing ndarray with new shape and strides, internal use
+    NDArray(std::shared_ptr<CompactArray<T>> handle, std::vector<size_t> shape, std::vector<size_t> strides, size_t offset = 0);
+
+    void compute_strides();
+
+    size_t size() const;
+    std::vector<size_t> shape() const;
+    std::vector<size_t> strides() const;
+    void print() const;
 };
--- src/backend_cpu.cc
+++ src/backend_cpu.cc
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <backend_cpu.hpp>
 
+/*
+ * Implementation of CompactArray methods.
+ */
+
 template <typename T>
 CompactArray<T>::CompactArray(size_t size) : data(size) {}
 
@@ -9,7 +13,7 @@ template <typename T>
 CompactArray<T>::CompactArray(const std::vector<T> &input) : data(input) {}
 
 template <typename T>
-CompactArray<T>::CompactArray(std::vector<T> &&input) : data(std::move(input)) {}
+CompactArray<T>::CompactArray(std::vector<T> &&input) : data{std::move(input)} {}
 
 template <typename T>
 size_t CompactArray<T>::size() const
@@ -39,4 +43,49 @@ const T *CompactArray<T>::ptr() const
     return data.data();
 }
 
+/*
+ * Implementation of NDArray methods.
+ */
+template <typename T>
+void NDArray<T>::compute_strides()
+{
+    // Need strides for each shape.size()
+    strides.resize(shape.size());
+    // Each prior dim must skip over all elements of later dims.
+    // Accumulate product
+    size_t dim_stride = 1;
+    for (int i = static_cast<int>(shape.size()) - 1; i >= 0; --i)
+    {
+        strides[i] = dim_stride;
+        dim_stride *= shape[i];
+    }
+}
+
+template <typename T>
+NDArray<T>::NDArray(const std::vector<size_t> &shape) : shape{shape}, offset{0}
+{
+    // Make a new shared pointer to compact array with total size
+    size_t total_size = 1;
+    for (size_t dim : shape)
+    {
+        total_size *= dim;
+    }
+    // Handle to zeroed compact array of total size
+    handle = std::make_shared<CompactArray<T>>(total_size);
+    compute_strides();
+}
+
+template <typename T>
+NDArray<T>::NDArray(std::vector<T> data, const std::vector<size_t> shape) : shape{std::move(shape)}, offset{0}
+{
+    // TODO
+}
+
+template <typename T>
+NDArray<T>::NDArray(std::shared_ptr<CompactArray<T>> handle, std::vector<size_t> shape, std::vector<size_t> strides, size_t offset) : handle{std::move(handle)}, shape{std::move(shape)}, strides{std::move(strides)}, offset{offset}
+{
+    // TODO
+}
+
 template class CompactArray<float>;
+template class NDArray<float>;
