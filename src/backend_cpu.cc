27d0482c3c9ec8a75db6aced671c59aa24911678
Dantun1 <daniel@tunyan.com>
2026-02-13 17:11:19 +0000

    feat: add broadcasting_shape helper and element-wise kernel/operators for NDArray

--- src/backend_cpu.cc
+++ src/backend_cpu.cc
@@ -296,6 +296,31 @@ void NDArray<T>::setitem_ewise(const std::vector<Slice> &slice_ranges, const NDA
 }
 
 template <typename T>
+DimVec broadcast_shape(const DimVec &s1, const DimVec &s2)
+{
+    int dims = static_cast<int>(std::max(s1.size(), s2.size()));
+    DimVec out(dims, 1);
+
+    // if dim
+    for (size_t k = 0; k < dims; ++k)
+    {
+        const size_t d1 = (k < s1.size()) ? s1[dims - 1 - k] : 1;
+        const size_t d2 = (k < s2.size()) ? s2[dims - 1 - k] : 1;
+
+        if (d1 == d2 || d1 == 1 || d2 == 1)
+        {
+            out[dims - 1 - k] = std::max(d1, d2);
+        }
+        else
+        {
+            throw std::invalid_argument("Cannot broadcast arrays: incompatible shapes");
+        }
+    }
+
+    return out;
+}
+
+template <typename T>
 NDArray<T> NDArray<T>::broadcast(const DimVec &new_shape) const
 {
     if (new_shape.size() < shape.size())
@@ -336,35 +361,33 @@ template <typename T, typename Op>
 NDArray<T> ewise_op_kernel(const NDArray<T> &a, const NDArray<T> &b, Op op)
 {
 
-    const auto &shape = a.get_shape();
+    const auto &shape = broadcast_shape(a.get_shape(), b.get_shape());
 
     NDArray<T> broadcasted_b = (shape == b.get_shape()) ? b : b.broadcast(shape);
+    NDArray<T> broadcasted_a = (shape == a.get_shape()) ? a : a.broadcast(shape);
     NDArray<T> target{shape};
 
-    const auto &astrides = a.get_strides();
+    const auto &astrides = broadcasted_a.get_strides();
     const auto &bstrides = broadcasted_b.get_strides();
-    const auto &tstrides = target.get_strides();
     size_t total_size = std::accumulate(shape.begin(), shape.end(), 1ULL, std::multiplies<size_t>());
 
     T *new_data = target.get_handle()->ptr();
-    const T *aptr = a.get_handle()->ptr();
+    const T *aptr = broadcasted_a.get_handle()->ptr();
     const T *bptr = broadcasted_b.get_handle()->ptr();
 
-    size_t t_idx = 0;
-    size_t a_idx = a.get_offset();
+    size_t a_idx = broadcasted_a.get_offset();
     size_t b_idx = broadcasted_b.get_offset();
 
     DimVec indices(shape.size(), 0);
     for (size_t i = 0; i < total_size; i++)
     {
-        new_data[t_idx] = op(aptr[a_idx], bptr[b_idx]);
+        new_data[i] = op(aptr[a_idx], bptr[b_idx]);
 
         for (int dim = static_cast<int>(shape.size()) - 1; dim >= 0; --dim)
         {
             indices[dim]++;
             a_idx += astrides[dim];
             b_idx += bstrides[dim];
-            t_idx += tstrides[dim];
             if (indices[dim] < shape[dim])
             {
                 break;
@@ -374,11 +397,45 @@ NDArray<T> ewise_op_kernel(const NDArray<T> &a, const NDArray<T> &b, Op op)
                 indices[dim] = 0;
                 a_idx -= shape[dim] * astrides[dim];
                 b_idx -= shape[dim] * bstrides[dim];
-                t_idx -= shape[dim] * tstrides[dim];
             }
         }
     }
     return target;
+}
+
+template <typename T>
+NDArray<T> ewise_add(const NDArray<T> &a, const NDArray<T> &b)
+{
+    return ewise_op_kernel(a, b, [](T a, T b)
+                           { return a + b; });
+}
+
+template <typename T>
+NDArray<T> ewise_sub(const NDArray<T> &a, const NDArray<T> &b)
+{
+    return ewise_op_kernel(a, b, [](T a, T b)
+                           { return a - b; });
+}
+
+template <typename T>
+NDArray<T> ewise_mul(const NDArray<T> &a, const NDArray<T> &b)
+{
+    return ewise_op_kernel(a, b, [](T a, T b)
+                           { return a * b; });
+}
+
+template <typename T>
+NDArray<T> ewise_pow(const NDArray<T> &a, const NDArray<T> &b)
+{
+    return ewise_op_kernel(a, b, [](T a, T b)
+                           { return std::pow(a, b); });
+}
+
+template <typename T>
+NDArray<T> ewise_div(const NDArray<T> &a, const NDArray<T> &b)
+{
+    return ewise_op_kernel(a, b, [](T a, T b)
+                           { return a / b; });
 }
 
 template <typename T, typename Op>
