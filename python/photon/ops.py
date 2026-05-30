9bab462e80cc7166d94f57fcb9ce29e5d61303c8
Dantun1 <daniel@tunyan.com>
2026-05-30 11:18:38 +0100

    Backward pass and Core ops

--- python/photon/ops.py
+++ python/photon/ops.py
@@ -1,2 +1,135 @@
+from typing import Any
+
 class Op:
-    pass
+    def __call__(self, *args):
+        """
+        Forward pass through a generic op. 
+
+        Each op implements its own  compute and gradient method.
+        """
+        from .tensor import Tensor
+        raw_inputs = [arg._data for arg in args]
+
+        # Run computation directly on ndarrays.
+        out_ndarray = self.compute(*raw_inputs)
+        
+        # req grad if any input needs grad
+        req_grad = any(arg._requires_grad for arg in args)
+        
+        # Construct new node in graph
+        return Tensor._from_ndarray(
+            out_ndarray, 
+            device=args[0]._device, 
+            requires_grad=req_grad, 
+            inputs=list(args),
+            op=self 
+            )
+    
+    def compute(self, *args):
+        """Computes the forward pass"""
+        raise NotImplementedError
+
+    def gradient(self, out_grad, out_node):
+        """Computes the backward pass"""
+        raise NotImplementedError
+
+class EwiseAdd(Op):
+    def compute(self, a_ndarr, b_ndarr):
+        return a_ndarr + b_ndarr
+    
+    def gradient(self, out_grad, out_node):
+        return out_grad, out_grad
+
+class ScalarAdd(Op):
+    def __init__(self, scalar):
+        self.scalar = scalar
+
+    def compute(self, a_ndarr):
+        return a_ndarr + self.scalar
+    
+    def gradient(self, out_grad, out_node):
+        return (out_grad,)
+
+class EwiseSub(Op):
+    def compute(self, a_ndarr, b_ndarr):
+        return a_ndarr - b_ndarr
+    
+    def gradient(self, out_grad, out_node):
+        return out_grad, -1 * out_grad
+
+class ScalarSub(Op):
+    def __init__(self, scalar):
+        self.scalar = scalar
+
+    def compute(self, a_ndarr):
+        return a_ndarr - self.scalar
+    
+    def gradient(self, out_grad, out_node):
+        return (out_grad,)
+    
+class EwiseMul(Op):
+    def compute(self, a_ndarr, b_ndarr):
+        return a_ndarr * b_ndarr
+    
+    def gradient(self, out_grad, out_node):
+        return out_node._inputs[1] * out_grad, out_node._inputs[0] * out_grad
+    
+class ScalarMul(Op):
+    def __init__(self, scalar):
+        self.scalar = scalar
+
+    def compute(self, a_ndarr):
+        return a_ndarr * self.scalar
+    
+    def gradient(self, out_grad, out_node):
+        return (out_grad * self.scalar,)
+
+class EwiseDiv(Op):
+    def compute(self, a_ndarr, b_ndarr):
+        return a_ndarr / b_ndarr
+    
+    def gradient(self, out_grad, out_node):
+        b = out_node._inputs[1]
+        grad_a = (1 / b) * out_grad
+        # Reuse output for gradient of b
+        grad_b = (-out_node / b) * out_grad 
+        
+        return grad_a, grad_b
+    
+class ScalarDiv(Op):
+    def __init__(self, scalar):
+        self.scalar = scalar
+
+    def compute(self, a_ndarr):
+        return a_ndarr / self.scalar
+    
+    def gradient(self, out_grad, out_node):
+        return (out_grad / self.scalar,)
+    
+
+class EwisePow(Op):
+    def compute(self, a_ndarr, b_ndarr):
+        return a_ndarr ** b_ndarr
+    
+    def gradient(self, out_grad, out_node):
+        #input tensors
+        a = out_node._inputs[0]
+        b = out_node._inputs[1]
+        
+        return b * a ** (b-1) * out_grad, a.log() * out_node * out_grad
+    
+class ScalarPow(Op):
+    def __init__(self, scalar):
+        self.scalar = scalar
+
+    def compute(self, a_ndarr):
+        return a_ndarr ** self.scalar
+    
+    def gradient(self, out_grad, out_node):
+        a = out_node._inputs[0]
+        return (self.scalar * a ** (self.scalar - 1) * out_grad,)
+
+#Implement the unary ops
+
+class Log(Op):
+    ...
--- python/photon/tensor.py
+++ python/photon/tensor.py
@@ -1,7 +1,9 @@
 import numpy as np
+from collections import deque
 
 from .backend_dispatcher import get_backend
-from .ops import Op
+from .ops import *
+from typing import Self
 
 class Tensor:
     """
@@ -46,8 +48,6 @@ class Tensor:
         elif isinstance(data, np.ndarray):
             self._data = backend.NDArray(data.flatten().tolist(),list(data.shape()))
 
-        self._data = backend.NDArray(data)
-
         # Initialise as isolated node, no gradient
         self._inputs = []
         self._op = None
@@ -61,12 +61,144 @@ class Tensor:
     def strides(self):
         return self._data.strides
     
+    def _topo(self):
+        """
+        Returns the topo ordering of the computational graph 
+        for the backward pass using iterative Kahn's algorithm.
+
+        Wanted to avoid recursion for large networks but will benchmark later
+
+        Returns:
+            list: ordering of all nodes
+        """
+        dependencies = {}
+        visited = set()
+        stack = [self]
+        
+        while stack:
+            curr = stack.pop()
+            if curr not in visited:
+                visited.add(curr)
+            
+                if curr not in dependencies:
+                    dependencies[curr] = 0
+                    
+                # Increment deps of all inputs
+                for node in curr._inputs:
+                    if node not in dependencies:
+                        dependencies[node] = 0
+                    dependencies[node] += 1
+                    stack.append(node)
+
+        topo_order = []
+        
+        # The final node has 0 deps 
+        queue = deque([self])
+        
+        # Pop off final node and decrement deps of inputs, 
+        # Iteratively adding 0 dep nodes.
+        while queue:
+            curr = queue.popleft()
+            topo_order.append(curr)
+            
+            for node in curr._inputs:
+                dependencies[node] -= 1
+        
+                if dependencies[node] == 0:
+                    queue.append(node)
+                    
+        return topo_order
+    
+    def backward(self):
+        """
+        Compute backward pass, populates gradients.
+        """
+        if not self._requires_grad:
+            raise RuntimeError("Gradient computation disabled for this tensor")
+        
+        # Initialise output gradients as ones (via numpy for now)
+        self.grad = Tensor(np.ones(self.shape), device=self._device, requires_grad=False)
+        topo_order = self._topo()
+        # Backprop gradients in topological order
+        for node in topo_order:
+            # Skip leaf nodes
+            if node._op is None:
+                continue 
+            
+            # Compute the grads to prop back
+            input_grads = node._op.gradient(node.grad, node)
+            
+            # init or accumulate input grads
+            for i, input_node in enumerate(node._inputs):
+                if not input_node._requires_grad:
+                    continue
+
+                if input_node.grad is None:
+                    input_node.grad = input_grads[i]
+                else:
+                    input_node.grad = input_node.grad + input_grads[i]
+    def log(self):
+        ...
+
+
+    
+    def __add__(self, other):
+        if isinstance(other, (int,float)):
+            return ScalarAdd(other)(self)
+        elif isinstance(other, Tensor):
+            return EwiseAdd()(self, other)
+        
+    def __sub__(self, other):
+        if isinstance(other, (int,float)):
+            return ScalarSub(other)(self)
+        elif isinstance(other, Tensor):
+            return EwiseSub()(self, other)
+    
+    def __mul__(self, other):
+        if isinstance(other, (int,float)):
+            return ScalarMul(other)(self)
+        elif isinstance(other, Tensor):
+            return EwiseMul()(self, other)
+    
+    def __truediv__(self, other):
+        if isinstance(other, (int,float)):
+            return ScalarDiv(other)(self)
+        elif isinstance(other, Tensor):
+            return EwiseDiv()(self, other)
+        
+    def __pow__(self, other):
+        if isinstance(other, (int,float)):
+            return ScalarPow(other)(self)
+        elif isinstance(other, Tensor):
+            return EwisePow()(self, other)
+    
+
+    
+    @classmethod
+    def _from_ndarray(cls, data, device: str = "cpu", requires_grad: bool = True, inputs: list[Self] = [], op: Op | None = None):
+        """
+        Alternate constructor taking in raw backend NDArray data, and comp graph info.
+        """
+        tens = cls.__new__(cls)
+        tens._data = data       
+        tens._device = device
+        tens._requires_grad = requires_grad
+        tens._inputs = inputs or []
+        tens._op = op
+
+        tens.grad = None
+
+        return tens
+    
     def __str__(self):
+        # Printing offloaded to numpy for now
         if self._device == "cpu":
             return str(np.array(self._data))
         elif self._device == "cuda":
             return str(self._data.numpy())
         raise RuntimeError("Impossible to print array as handle is invalid")
+    
+    
     
 
     
